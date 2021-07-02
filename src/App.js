91ce6b76596706f08fc42c48c2b3a0225cda743d
Tomasz Rosik <Bosik27@gmail.com>
2021-07-02 02:44:14 +0200

    Init Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Organizations from './components/Organizations/Organizations';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<Organizations />
+		</div>
+	);
+};
+
+export default App;
